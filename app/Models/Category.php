69b19cb6572626b2ea1eafc04965e54c63517e82
vaskaraminashvili <vaska.r02@gmail.com>
2023-09-07 17:57:05 +0400

    lesson 6 finished

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,4 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+
+    /**
+     * Get all of the products for the Category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
